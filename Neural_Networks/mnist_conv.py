38729ca5c17df5f5ebeed969b29bbbad2b48f45b
Srish V <srishv2010@gmail.com>
2023-10-06 22:36:38 +0100

    Version 12 (Working)

--- Neural_Networks/mnist_conv.py
+++ Neural_Networks/mnist_conv.py
@@ -7,31 +7,29 @@ from activation_functions import Sigmoid
 from losses import BinaryCrossEntropy
 from networks import Sequential
 
+
 def preprocess_data(x, y, limit):
-    zero_index = np.where(y == 0)[0][:limit]
-    one_index = np.where(y == 1)[0][:limit]
-    all_indices = np.hstack((zero_index, one_index))
-    all_indices = np.random.permutation(all_indices)
-    x, y = x[all_indices], y[all_indices]
     x = x.reshape(len(x), 1, 28, 28)
     x = x.astype("float32") / 255
     y = np_utils.to_categorical(y)
-    y = y.reshape(len(y), 2, 1)
-    return x, y
+    y = y.reshape(len(y), 10, 1)
+    return x[:limit], y[:limit]
+
 
 # load MNIST from server, limit to 100 images per class since we're not training on GPU
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, y_train = preprocess_data(x_train, y_train, 100)
-x_test, y_test = preprocess_data(x_test, y_test, 100)
+x_train, y_train = preprocess_data(x_train, y_train, 10000)
+x_test, y_test = preprocess_data(x_test, y_test, 10000)
 
-# neural network
 network = Sequential([
-    Convolutional2D((1, 28, 28), (3, 3), 5),
+    Convolutional2D((1, 28, 28), (5, 5), 5),
     Sigmoid(),
-    Reshape((5 * 26 * 26, 1)),
-    Dense(5 * 26 * 26, 100),
+    Convolutional2D((5, 24, 24), (3, 3), 5),
     Sigmoid(),
-    Dense(100, 2),
+    Reshape((22 * 22 * 5, 1)),
+    Dense(22 * 22 * 5, 100),
+    Sigmoid(),
+    Dense(100, 10),
     Sigmoid()
 ], loss_function=BinaryCrossEntropy)
 
